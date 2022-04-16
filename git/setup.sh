99c727525d594933963591e30e6a2f9c3a388062
{name} <{email}>
2022-04-16 15:37:00 +0200

    Improve git config setup by passing name and email

--- git/setup.sh
+++ git/setup.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-ln -sf "$PWD/.gitconfig" "$HOME/.gitconfig"
-ln -sf "$PWD/.gitignore_global" "$HOME/.gitignore_global"
+if [ $# -lt 2 ]; then
+    echo "Provide name (arg1) and email (arg2) in order to setup git config"
+    exit 1
+fi
+
+TEMP_FILE=".tmp_gitconfig"
+
+# Replace placeholders
+cp .gitconfig $TEMP_FILE
+sed -i "s/{name}/$1/g" "$TEMP_FILE"
+sed -i "s/{email}/$2/g" "$TEMP_FILE"
+
+# Copy config
+mv $TEMP_FILE "$HOME/.gitconfig"
+cp "$PWD/.gitignore_global" "$HOME/.gitignore_global"
