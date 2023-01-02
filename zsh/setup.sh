7996d70e0ac953a69b5ebac70661b62c9223c4eb
Michał Brotoń <michal.broton.ns@gmail.com>
2023-01-02 22:42:15 +0100

    Update zsh setup.py and config file

--- zsh/setup.sh
+++ zsh/setup.sh
@@ -9,8 +9,5 @@ chsh -s $(which zsh) $USER
 # download oh-my-zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 
-# install p10k?
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-
 # copy config
 cp "$PWD/.zshrc" "$HOME/.zshrc"
